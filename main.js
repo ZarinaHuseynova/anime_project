ff1a734c095a3c5738b387049540200ed0860ff6
ZarinaHuseynova <huseynovazarina653@gmail.com>
2022-01-16 21:12:23 +0400

    Create Anime project

---
+++ main.js
@@ -0,0 +1,42 @@
+// var myCarousel = document.querySelector('#carouselExampleControls')
+// var carousel = new bootstrap.Carousel(myCarousel)
+
+// var swiper = new Swiper(".mySwiper", {
+//     spaceBetween: 30,
+//     effect: "fade",
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
+
+
+   //Initialize Swiper
+  
+      // var swiper = new Swiper(".mySwipper__slider", {
+      //   spaceBetween: 30,
+      //   effect: "fade",
+      //   navigation: {
+      //     nextEl: ".owl__next",
+      //     prevEl: ".owl__prev"
+      //   },
+      // //   pagination: {
+      // //     el: ".swiper-pagination",
+      // //     clickable: true
+      // //   },
+      //   on: {
+      //     slideChange: function () {
+      //       swiper.slides.forEach((element) => {
+      //         element.firstElementChild.classList.add("passive");
+      //         element.firstElementChild.classList.remove("active");
+      //       });
+      //       swiper.slides[swiper.activeIndex].firstElementChild.classList.add(
+      //         "active"
+      //       );
+      //     }
+      //   }
+      // });
